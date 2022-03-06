f8c03750fa13dc34e17c5262774472bec3d1cbab
jesse tripp <tripp.pdf@gmail.com>
2022-03-06 12:18:48 -0500

    Final Commit - .java, JUnit, and "Opinion Paragraph" included

--- Assignment 5 - PE 12.12/src/Exercise12_12.java
+++ Assignment 5 - PE 12.12/src/Exercise12_12.java
@@ -1,42 +1,62 @@
+// ==============================================
+// Jesse Tripp
+// COSC 211
+// Assignment 5 - PE 12.12
+// Refactored program after Text I/O chapter.
+// ==============================================
+
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Exercise12_12 {
-  /** Main method */
   public static void main(String[] args) throws Exception {
-    // Check command line parameter usage
+    success(args);
+    File sourceFile = fileExistsCheck(args);
+    StringBuilder buffer = new StringBuilder();
+    formatFile(sourceFile, buffer);
+    writeBuffer(sourceFile, buffer);
+  }
+
+  private static void writeBuffer(File sourceFile, StringBuilder buffer)
+      throws FileNotFoundException {
+    try (PrintWriter output = new PrintWriter(sourceFile);) {
+      output.print(buffer.toString());
+    }
+  }
+
+  private static void formatFile(File sourceFile, StringBuilder buffer)
+      throws FileNotFoundException {
+    try (Scanner input = new Scanner(sourceFile)) {
+      while (input.hasNext()) {
+        String s = input.nextLine();
+        String s1 = s.trim();
+        if (s1.charAt(0) == '{' && s1.length() > 1) {
+          buffer.append(" {");
+          buffer.append("\r\n" + s.replace('{', ' '));
+        } else
+          buffer.append("\r\n" + s);
+      }
+    }
+  }
+
+  private static File fileExistsCheck(String[] args) {
+    File sourceFile = new File(args[0]);
+    try {
+      if (!sourceFile.exists()) {
+        throw new FileNotFoundException();
+      }
+    } catch (Exception ex) {
+      System.out.println("Source file " + args[0] + " not exist");
+    }
+    return sourceFile;
+  }
+
+  private static void success(String[] args) {
     if (args.length != 1) {
       System.out.println("Usage: java Exercise12_12 filename");
       System.exit(1);
     }
-
-    // Check if source file exists
-    File sourceFile = new File(args[0]);
-    if (!sourceFile.exists()) {
-      System.out.println("Source file " + args[0] + " not exist");
-      System.exit(2);
-    }
-
-    StringBuilder buffer = new StringBuilder();
-    Scanner input = new Scanner(sourceFile);
-
-    while (input.hasNext()) {
-      String s = input.nextLine();
-      String s1 = s.trim();
-      if (s1.charAt(0) == '{') {
-        buffer.append(" {");
-        if (s1.length() > 1)
-          buffer.append("\r\n" + s.replace('{', ' '));
-      } else
-        buffer.append("\r\n" + s);
-    }
-
-    input.close();
-
-    // Write buffer into the file
-    PrintWriter output = new PrintWriter(sourceFile);
-    output.print(buffer.toString());
-    output.close();
   }
 }
---
+++ Assignment 5 - PE 12.12/src/Exercise12_12Test.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+
+class Exercise12_12Test {
+
+  // NOT SURE HOW TO JUNIT TEST THIS PROGRAM, AT ALL
+  // NO HELP OFFERED IN DISCUSSION BOARD POST FROM CLASSMATES
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
